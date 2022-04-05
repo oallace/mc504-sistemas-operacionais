8d32fbca7817ac8018715bd58d2d8503208ec66b
wallace g s lima <wallacegsantoslima@gmail.com>
2022-04-05 20:56:03 -0300

    adiciona imagem

--- proj01/filto.c
+++ proj01/filto.c
@@ -1,4 +1,10 @@
 // Autor: Wallace Lima, RA 195512
 // Aplicador de filtros a uma imagem  utilizando pthreads
-// Formato de imagem de Entrada?
+// Formato de imagem de Entrada: BMP
+
+// variáveis
+
+int filtro = [[1,0,0], [0,1,0], [0,0,1]];
+
+// Abrindo uma imagem BMP
 
